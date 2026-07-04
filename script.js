b73b5e48a447d85b907b30701e28bcc9929366dc
hyannu <annurao9560@gmail.com>
2026-07-04 11:35:16 +0530

    Completed JavaScript inheritance practice

--- script.js
+++ script.js
@@ -1,7 +1,34 @@
-const student = {
+/*const student = {
     fulName: "Archana Rao",
     marks: 94.4,
     printMarks: function () {
         console.log("marks = ", this.marks);
     }
 }
+    */
+
+
+
+class Person {
+    constructor (name) {
+        console.log("enter parent constructor");
+        this.species = "homo sapiens";
+        this.name = name;
+    }
+
+eat() {
+    console.log("eat");
+   }
+}
+
+class Engineer extends Person {
+    constructor(name){
+        super(name); // to invoke parent class constructor
+    }
+    work() {
+        console.log("solve problem, build something");
+    }
+}
+
+let engObj = new Engineer("Archana Rao");
+
