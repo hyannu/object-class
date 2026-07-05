b403280756f9ed51c20dc7cb928bdc02f1331b45
hyannu <annurao9560@gmail.com>
2026-07-05 20:04:50 +0530

    synchronous programming call back function

--- callback/script.js
+++ callback/script.js
@@ -1,4 +1,13 @@
-function  hello(){
+/*function  hello(){
     console.log("hello");
 }
 setTimeout(hello, 2000);//timeout
+*/
+
+
+
+/*setTimeout(() => {
+console.log("hello");
+},4000);//timeout;
+*/
+ 
