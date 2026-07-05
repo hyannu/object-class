be3ae9355267b8bb67e4966b6f234464c9f56d49
hyannu <annurao9560@gmail.com>
2026-07-05 23:08:06 +0530

    nesting key word

--- callback/script.js
+++ callback/script.js
@@ -11,3 +11,23 @@ console.log("hello");
 },4000);//timeout;
 */
  
+
+//Synchronous programming call back function
+
+/*function sum(a,b) {
+    console.log(a+b);
+}
+function calculator(a,b,sumCallback){
+    sumCallback(a,b);
+}
+calculator(1,2,(a,b) =>{
+    console.log(a+b);
+});
+*/
+
+const hello = () =>{
+    console.log("hello");
+};
+setTimeout(hello,3000);
+
+
