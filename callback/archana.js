c23e86bb292d6d3010d6ccc44fe0823223a7c1dc
hyannu <annurao9560@gmail.com>
2026-07-06 18:32:06 +0530

    async - await

--- callback/archana.js
+++ callback/archana.js
@@ -77,12 +77,38 @@ getData(1, () => {
 */
 
 //permises
-let permise  = new Promise((resolve, reject) =>{
+/*let permise  = new Promise((resolve, reject) =>{
     console.log("I am a promise");
     // resolve("success");
     reject("sum error");
 });
+*/
 
+/*function asyncFunc1() {
+   return new Promise ((resolve, reject) =>{
+    setTimeout(() => {
+        console.log("some data1");
+        resolve("success");
+    },4000);
+   }) ;
+}
+function asyncFunc2() {
+   return new Promise ((resolve, reject) =>{
+    setTimeout(() => {
+        console.log("some data1");
+        resolve("success");
+    },4000);
+   }) ;
+}
+
+console.log("fetching data1...");
+let p1 = asyncFunc1();
+p1.then((res) => {
+    console.log("fetching data2...");
+    let p2= asyncFunc2();
+    p2.then((res) => {});
+});
+*/
 
 
 
